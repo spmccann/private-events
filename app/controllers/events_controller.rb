ba5ed3a3ac1c7ac9b92494e8f96ba79b2e15cc0d
Scott <scottpmccann@gmail.com>
2022-08-23 12:41:57 -0700

    nav and scopes for past and upcoming

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,10 +1,6 @@
 class EventsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
 
-  def index
-    @events = Event.all
-  end
-
   def new
     @event = Event.new
   end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,7 @@
 class Event < ApplicationRecord
   belongs_to :user, foreign_key: 'creator_id', class_name: 'User'
   has_many :attended_events
+
+  scope :upcoming_events, -> { where('date >= ?', Date.today) }
+  scope :past_events, -> { where('date < ?', Date.today) }
 end
